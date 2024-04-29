d6d8bae0a52c570c1828c7435104433a8409aa69
Dang Tai <taidtps27144@fpt.edu.vn>
2024-04-29 16:18:41 +0700

    feat: other get functions
    get film by id, get years, get rates, get top rated film, get random films

--- src/api/controllers/filmController.js
+++ src/api/controllers/filmController.js
@@ -18,14 +18,14 @@ exports.getFilms = catchAsync(async (req, res, next) => {
   // get id of categories
   let idCategories;
   if (categories) {
-    const resultCategories = await Categories.findOne({ name: categories });
+    const resultCategories = await Categories.findOne({ slug: categories });
     idCategories = resultCategories._id;
   }
 
   // get id of countries
   let idCountries;
   if (countries) {
-    const resultCountries = await Countries.findOne({ name: countries });
+    const resultCountries = await Countries.findOne({ slug: countries });
     idCountries = resultCountries._id;
   }
 
@@ -90,11 +90,120 @@ exports.getFilmBySlug = catchAsync(async (req, res, next) => {
     return next(new AppError("No film found", 404));
   }
 
-  const episodes = await Episodes.find({ film_id: film._id });
+  const episodes = await Episodes.find({ film_id: film._id }, projection);
   film.episodes = episodes;
 
   res.status(200).json({
     status: "success",
     data: film,
+  });
+});
+
+// @desc  Get film by id
+// @route GET /api/v1/films/:id
+// @access Public
+
+exports.getFilmById = catchAsync(async (req, res, next) => {
+  const projection = { __v: 0 };
+
+  const film = await Film.findById(req.params.id, projection)
+    .populate("categories", "-__v")
+    .populate("countries", "-__v")
+    .exec();
+
+  if (!film) {
+    return next(new AppError("No film found", 404));
+  }
+
+  const episodes = await Episodes.find({ film_id: film._id }, projection);
+  film.episodes = episodes;
+
+  res.status(200).json({
+    status: "success",
+    data: film,
+  });
+});
+
+// @desc  Get years of films
+// @route GET /api/v1/films/years
+// @access Public
+
+exports.getYears = catchAsync(async (req, res, next) => {
+  const years = await Film.distinct("year");
+
+  res.status(200).json({
+    status: "success",
+    data: years,
+  });
+});
+
+// @desc  Get rates
+// @route GET /api/v1/films/rates
+// @access Public
+
+exports.getRates = catchAsync(async (req, res, next) => {
+  const rates = await Film.distinct("rate");
+
+  res.status(200).json({
+    status: "success",
+    data: rates,
+  });
+});
+
+// @desc  Get top rated films
+// @route GET /api/v1/films/top-rated
+// @access Public
+
+exports.getTopRatedFilms = catchAsync(async (req, res, next) => {
+  // projection
+  const projection = {
+    name: 1,
+    slug: 1,
+    origin_name: 1,
+    thumb_url: 1,
+    poster_url: 1,
+    year: 1,
+    rate: 1,
+  };
+
+  const films = await Film.find({}, projection).sort({ rate: -1 });
+
+  res.status(200).json({
+    status: "success",
+    data: films,
+  });
+});
+
+// @desc  Get random films
+// @route GET /api/v1/films/random
+// @access Public
+
+exports.getRandomFilms = catchAsync(async (req, res, next) => {
+  // projection
+  const projection = {
+    name: 1,
+    slug: 1,
+    origin_name: 1,
+    thumb_url: 1,
+    poster_url: 1,
+    year: 1,
+    rate: 1,
+    categories: 1,
+    time: 1,
+  };
+
+  // get 5 random films with projection
+  const films = await Film.aggregate([{ $sample: { size: 5 } }])
+    .lookup({
+      from: "categories",
+      localField: "categories",
+      foreignField: "_id",
+      as: "categories",
+    })
+    .project(projection);
+
+  res.status(200).json({
+    status: "success",
+    data: films,
   });
 });
